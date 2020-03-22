cbc36d14b5273ea2158cddb1637aa5a284f844e4
petermkr <mail@peter-kroiss.de>
2020-03-22 20:13:32 +0100

    Create roles component

--- ui/src/app/roles/client/client.service.ts
+++ ui/src/app/roles/client/client.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Product} from "./client.component";
-import {environment} from "../../environments/environment";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
