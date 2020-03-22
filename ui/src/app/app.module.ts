fb0304374b5e68836d24b4753dacbb666af0ee7c
petermkr <mail@peter-kroiss.de>
2020-03-22 01:02:57 +0100

    Update node dependencies

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,9 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import {HttpClientModule} from "@angular/common/http";
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
 
 @NgModule({
   declarations: [
