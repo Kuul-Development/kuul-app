a2985b291d49029454d8cbadaf3cb47525d63ecb
petermkr <mail@peter-kroiss.de>
2020-03-22 01:38:51 +0100

    Add ng-bootstrap and basic tab view

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ProductComponent } from './product/product.component';
 import {HttpClientModule} from "@angular/common/http";
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import {BrowserModule} from "@angular/platform-browser";
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
