a112e2cbeb5e22f9acaa9f9f4d2b4cfaaf39aec3
petermkr <mail@peter-kroiss.de>
2020-03-24 23:04:51 +0100

    Add basic test infrastructure and failing test

--- server/src/main/java/com/kuul/bc/product/resource/SupplierViewResource.java
+++ server/src/main/java/com/kuul/bc/product/resource/SupplierViewResource.java
@@ -10,7 +10,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 /**
  * Resource class for the supplier view
@@ -40,11 +40,14 @@ public class SupplierViewResource {
     @Produces(MediaType.APPLICATION_JSON)
     public static Response updateRadioState(@QueryParam("orderId") long orderId) {
         ClientViewResource client = new ClientViewResource();
-        Order order = client.getOrders().stream().filter(o -> o.getOrderId() == orderId).collect(Collectors.toList()).get(0);
-        order.setDone(true);
+        Optional<Order> order = findOrderById(client.getOrders(), orderId);
+        order.ifPresent(o -> o.setDone(true));
         return Response
                 .ok(client.getOrders(), MediaType.APPLICATION_JSON)
                 .build();
     }
 
+    static Optional<Order> findOrderById(List<Order> orders, long orderId)  {
+        return orders.stream().filter(o -> o.getOrderId() == orderId).findFirst();
+    }
 }
---
+++ server/src/test/java/com/kuul/bc/product/resource/SupplierViewResourceTest.java
@@ -0,0 +1,11 @@
+package com.kuul.bc.product.resource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class SupplierViewResourceTest {
+    @Test
+    void addition() {
+        assertEquals(2, 3);
+    }
+}
