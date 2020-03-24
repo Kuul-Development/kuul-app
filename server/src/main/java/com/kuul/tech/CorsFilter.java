6a71f09dcf0434ac48077bec07cf7f2c53895def
petermkr <mail@peter-kroiss.de>
2020-03-24 22:16:52 +0100

    Commit syntax error to check travis

--- server/src/main/java/com/kuul/tech/CorsFilter.java
+++ server/src/main/java/com/kuul/tech/CorsFilter.java
@@ -2,7 +2,7 @@ package com.kuul.tech;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
-import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.ContainerResponseFilter
 import javax.ws.rs.ext.Provider;
 
 @Provider
