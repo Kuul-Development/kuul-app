bfa040621a765d364373c13305c90b775b47b2e9
petermkr <mail@peter-kroiss.de>
2020-03-29 15:14:39 +0200

    Add test endpoint

--- server/src/main/java/com/kuul/bc/product/facade/SalesmanViewFacade.java
+++ server/src/main/java/com/kuul/bc/product/facade/SalesmanViewFacade.java
@@ -44,6 +44,15 @@ public class SalesmanViewFacade {
                        .build();
     }
 
+    @GET
+    @Path("testendpoint")
+    public Response getTestEndpoint() {
+        Salesman salesman = new Salesman();
+        salesman.setName("testName");
+        return Response.ok(salesman, MediaType.APPLICATION_JSON)
+                .build();
+    }
+
     /**
      * Add a product to the catalogue of a certain salesman
      */
