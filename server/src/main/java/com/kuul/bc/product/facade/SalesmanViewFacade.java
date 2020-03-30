33a5697cf3c3a6cc877fb7a76f440343d2f8a6f7
petermkr <mail@peter-kroiss.de>
2020-03-30 10:14:41 +0200

    Switch to glassfish container deployment

--- server/src/main/java/com/kuul/bc/product/facade/SalesmanViewFacade.java
+++ server/src/main/java/com/kuul/bc/product/facade/SalesmanViewFacade.java
@@ -48,7 +48,7 @@ public class SalesmanViewFacade {
     @Path("testendpoint")
     public Response getTestEndpoint() {
         Salesman salesman = new Salesman();
-        salesman.setName("testName4");
+        salesman.setName("testName7");
         return Response.ok(salesman, MediaType.APPLICATION_JSON)
                 .build();
     }
--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  serverBaseUrl: 'http://kuulserverapp-env.eba-mp6wwhfk.us-east-1.elasticbeanstalk.com/rest',
+  serverBaseUrl: 'http://kuulapp-env.eba-jq5zuybp.us-east-1.elasticbeanstalk.com/rest',
   production: true
 };
