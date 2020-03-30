a00b2d1d4e57458dae90e50184348c1e2fbfee42
petermkr <mail@peter-kroiss.de>
2020-03-30 22:12:49 +0200

    Try allowing every site in corsfilter

--- server/src/main/java/com/kuul/tech/CorsFilter.java
+++ server/src/main/java/com/kuul/tech/CorsFilter.java
@@ -10,7 +10,7 @@ public class CorsFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext request, ContainerResponseContext response) {
-        response.getHeaders().add("Access-Control-Allow-Origin", "http://testbucket-pekr.s3-website-us-east-1.amazonaws.com/");
+        response.getHeaders().add("Access-Control-Allow-Origin", "*");
         response.getHeaders().add("Access-Control-Allow-Methods", "GET, PUT, OPTIONS");
     }
 }
